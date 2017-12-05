c0d37f05b94f3970addf75ade94c992203877db7
Valbrand <daniel@dvalbrand.com>
2017-12-05 20:24:29 -0300

    Implemented first version of mobx store

---
+++ src/state/LetsChatStore.js
@@ -0,0 +1,39 @@
+import { action, extendObservable, observable } from "mobx";
+
+export default class LetsChatStore {
+  constructor() {
+    extendObservable(this, {
+      _chatRooms: observable.map({}),
+      get chatRooms() {
+        return this._chatRooms.toJS();
+      },
+
+      currentUser: null,
+      messages: null,
+      selectedChatRoom: null,
+
+      selectChatRoom: action(roomId => {
+        if (roomId !== this.selectedChatRoom) {
+          this.messages = null;
+        }
+        this.selectedChatRoom = roomId;
+      }),
+      changeUser: action(user => {
+        this.currentUser = user;
+      }),
+      addChatRoom: action((roomId, room) => {
+        this.chatRooms.set(roomId, room);
+      }),
+      removeChatRoom: action(roomId => {
+        this.chatRooms.delete(roomId);
+      }),
+      addMessages: action(messages => {
+        if (this.messages === null) {
+          this.messages = messages;
+        } else {
+          this.messages = this.messages.concat(messages);
+        }
+      })
+    });
+  }
+}
